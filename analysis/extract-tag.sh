c98bbb8826666c8ed973f3c569c4bb69997cccee
hupili <hpl1989@gmail.com>
2012-11-25 15:04:30 +0800

    extract tag stat

---
+++ analysis/extract-tag.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p data
+
+sql="sqlite3 srfe.db"
+
+$sql 'select * from tag' > data/tag.schema
+
+for ((tag_id=1;tag_id<=9;tag_id++))
+do
+	$sql "select username from msg,msg_tag where msg_id=msg.id and tag_id=$tag_id" > data/tag.id$tag_id
+	cd data
+	sort tag.id$tag_id | uniq -c | sort -nr > tag.id$tag_id.freq
+	cd -
+done
+
+exit 0 
