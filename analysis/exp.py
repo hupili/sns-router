b7c444582553255d9de6fb8d9ff8635c90ed6c3f
hupili <hpl1989@gmail.com>
2012-11-25 16:45:22 +0800

    preprocessing

--- analysis/exp.py
+++ analysis/exp.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Dump sqlite3 data to python pickle
-# About:
-# 1.5G, 1 minute
-
 import sys
 sys.path.append('../bottle')
 sys.path.append('../snsapi')
@@ -27,6 +23,6 @@ import sqlite3
 if __name__ == '__main__':
     import time
     begin = time.time()
-    message = Serialize.loads(open('message.pickle').read())
+    message = Serialize.loads(open('workspace.pickle').read())
     end = time.time()
     print "Load finish. Time elapsed: %.3f" % (end - begin)
---
+++ analysis/pre.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append('../bottle')
+sys.path.append('../snsapi')
+import snsapi
+from snsapi.snspocket import SNSPocket
+from snsapi.platform import SQLite
+from snsapi import utils as snsapi_utils
+from snsapi import snstype
+# On my server, pickle uses 24s to load 
+# cPickle uses 6s to load. 
+# very significant
+#from snsapi.utils import Serialize
+import cPickle as Serialize
+from snsapi.snsbase import SNSBase
+from snsapi.snslog import SNSLog as logger
+
+import base64
+import hashlib
+import sqlite3
+
+if __name__ == '__main__':
+    import time
+
+    # load
+    begin = time.time()
+    message = Serialize.loads(open('message.pickle').read())
+    end = time.time()
+    print "Load finish. Time elapsed: %.3f" % (end - begin)
+
+    # Preprocessing
+    ml = message['message_list']
+    md = {}
+    for m in ml:
+        md[m.msg_id] = m
+    message['dict_msg'] = md 
+
+    tl = message['tag_list']
+    td = {}
+    td_r = {}
+    for (msg_id, tag_id) in tl:
+        #print "msg_id: %d, tag_id: %d" % (msg_id, tag_id)
+        if not msg_id in td:
+            td[msg_id] = {}
+        td[msg_id][tag_id] = 1
+        if not tag_id in td_r:
+            td_r[tag_id] = {}
+        td_r[tag_id][msg_id] = 1
+    #tl_r = [(t(2), t(1)) for t in tl]
+    #dl = dict(tl)
+    #dl_r = dict(tl_r)
+    message['dict_msg2tag'] = td
+    message['dict_tag2msg'] = td_r
+
+    # save 
+    begin = time.time()
+    open('workspace.pickle', 'w').write(Serialize.dumps(message))
+    end = time.time()
+    print "Save finish. Time elapsed: %.3f" % (end - begin)
