4ee7f5d1a6adf7de5ac8065bdf93297404d3196b
hupili <hpl1989@gmail.com>
2013-05-30 22:00:43 +0800

    collapse text

--- views/polish.js
+++ views/polish.js
@@ -1,8 +1,6 @@
 $(function(){
 	$('.message_body').map(function(){
-		//$(this).html($(this).html().replace(/http:\/\/[a-z0-9:\/%\.\-]+[^a-z0-9:\/%\.\-]/i, function(link){ 
-		//	if (r = /(http:\/\/([a-z0-9:%\.\-]+)\/[a-z0-9:\/%\.\-]+)([^a-z0-9:\/%\.\-])/i.exec(link)){
-		//	//if (r=/http:\/\/[a-z0-9:\/%\.\-]+[^a-z0-9:\/%\.\-]/.exec(link)){
+		// Extract and truncate link
 		$(this).html($(this).html().replace(/http:\/\/[a-z0-9:\/%\.\-&?=]+/i, function(link){ 
 			if (r = /(http:\/\/([a-z0-9:%\.\-]+)\/[a-z0-9:\/%\.\-&?=]+)/i.exec(link)){
 			//if (r=/http:\/\/[a-z0-9:\/%\.\-]+[^a-z0-9:\/%\.\-]/.exec(link)){
@@ -14,6 +12,38 @@ $(function(){
 				return link;
 			}
 		}));
+
+		// Collapse long texts
+		$(this).css('text-overflow', 'ellipsis');
+		$(this).css('line-height', '1.5em');
+		//$(this).css('white-space', 'nowrap');
+		$(this).css('overflow', 'hidden');
+		// Analyze min height
+		var _current_h = $(this).css('height').replace('px', '');
+		$(this).css('height', '4.5em');
+		var _expect_h = $(this).css('height').replace('px', '');
+		var _min_h = 0; 
+		if (parseInt(_current_h) < parseInt(_expect_h)){
+			_min_h = _current_h + 'px';
+		} else {
+			_min_h = _expect_h + 'px';
+		}
+		//alert(_min_h + "," + _expect_h + "," + _current_h);
+		$(this).attr('_expand', 'false');
+		$(this).attr('_min_h', _min_h);
+		$(this).css('height', _min_h);
+		$(this).css('background', '#f0f0f0');
+		$(this).click(function () {
+			if ($(this).attr('_expand') == 'false') {
+				$(this).attr('_expand', 'true');
+				$(this).css('height', 'auto');
+				$(this).css('background', '#ffffff');
+			} else {
+				$(this).attr('_expand', 'false');
+				$(this).css('height', $(this).attr('_min_h'));
+				$(this).css('background', '#f0f0f0');
+			}
+		});
 	});
 	$('.truncated_link').map(function(){
 		$(this).mouseover(function(){
