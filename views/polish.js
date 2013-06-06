1d69f7a9630af0b82ea2f6dec6afaab27390e3bd
hupili <hpl1989@gmail.com>
2013-06-06 21:28:11 +0800

    regex upgrade; remove link expansion

--- views/polish.js
+++ views/polish.js
@@ -1,8 +1,8 @@
 $(function(){
 	$('.message_body').map(function(){
 		// Extract and truncate link
-		$(this).html($(this).html().replace(/http:\/\/[a-z0-9:\/%\.\-&?=]+/i, function(link){ 
-			if (r = /(http:\/\/([a-z0-9:%\.\-]+)\/[a-z0-9:\/%\.\-&?=]+)/i.exec(link)){
+		$(this).html($(this).html().replace(/http:\/\/[a-z0-9:\/%\.\-&?=_;~]+/i, function(link){ 
+			if (r = /(http:\/\/([a-z0-9:%\.\-]+)\/[a-z0-9:\/%\.\-&?=_;~]+)/i.exec(link)){
 			//if (r=/http:\/\/[a-z0-9:\/%\.\-]+[^a-z0-9:\/%\.\-]/.exec(link)){
 				return '<a class="truncated_link" href="' + r[1] 
 						+ '" title="' + r[1] 
@@ -45,12 +45,14 @@ $(function(){
 			}
 		});
 	});
-	$('.truncated_link').map(function(){
-		$(this).mouseover(function(){
-			$(this).html($(this).attr('href'));	
-		});
-		$(this).mouseout(function(){
-			$(this).html($(this).attr('short_href'));	
-		});
-	});
+	
+	// mouse over to expand the link. canceled function
+	//$('.truncated_link').map(function(){
+	//	$(this).mouseover(function(){
+	//		$(this).html($(this).attr('href'));	
+	//	});
+	//	$(this).mouseout(function(){
+	//		$(this).html($(this).attr('short_href'));	
+	//	});
+	//});
 })
